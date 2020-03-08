dac2a384d2e854f7df522aedb627c6eb0c374e2f
MariusTarnaru <mtarnaru@yahoo.com>
2020-03-08 14:10:42 +0200

    Controller with username and password verified

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,10 +1,19 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.hibernate.usertype.UserType;
 import sample.entity.User;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 
 public class Controller {
@@ -14,18 +23,28 @@ public class Controller {
     @FXML
     private PasswordField password;
 
-    public void loginButtonIsClicked() {
-        User user = new User(username.getText(), password.getText());
-      /*
-        if(userExists(userList, user)){
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setContentText("You have logged in succesfully !");
-            alert.show();
+
+    public void loginButtonIsClicked(ActionEvent event) throws IOException {
+        User user = new User();
+        user.setUsername(username.getText());
+        user.setPassword(password.getText());
+
+        UserVerifier userVerifier = new UserVerifier();
+        if (userVerifier.userExist(user) && isUserEmployeType(user)) {
+            Parent parent = FXMLLoader.load(getClass().getResource("emploee.fxml"));
+            Scene scene = new Scene(parent);
+            Stage appStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.show();
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("You have logged in succesfully !");
+                alert.show();
+
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setContentText("You have failed !!!!");
             alert.show();
         }
-    }*/
+    }
 
 }
