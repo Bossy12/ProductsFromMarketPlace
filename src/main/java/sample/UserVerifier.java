c85a8bec4b5af35e6f6bc4fecba80a22d62d3560
sabiutayo <sabiutayo@yahoo.com>
2020-03-08 14:08:52 +0200

    added isUserEmployeeType and reviewed userExist

--- src/main/java/sample/UserVerifier.java
+++ src/main/java/sample/UserVerifier.java
@@ -1,24 +1,28 @@
 package sample;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import sample.entity.User;
 
-import java.util.List;
-
-
 public class UserVerifier {
 
-    private List<User> users;
     private Session session = HibernateUtil.getSession();
 
     public boolean userExist(User userToVerify) {
-
-        Query<User> query = session.createQuery("select u from User u where username = :username and  password = :password", User.class);
+        Query<User> query = session.createQuery(
+                "select u from User u where username = :username and  password = :password", User.class);
         query.setParameter("username", userToVerify.getUsername());
         query.setParameter("password", userToVerify.getPassword());
-       return query.getSingleResult()!=null;
+        return query.getResultList().size() > 0;
+    }
+
+    public boolean isUserEmployeeType(User userToVerify) {
+        Query<User> query = session.createQuery(
+                "select u from User u where username = :username and  password = :password", User.class);
+        query.setParameter("username", userToVerify.getUsername());
+        query.setParameter("password", userToVerify.getPassword());
+        return "employee".equals(query.getSingleResult().getUserType());
+
     }
 
 
