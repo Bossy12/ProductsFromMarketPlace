97c0b4613cf3c8d861f2a2e6bd26358b8e41d052
MariusTarnaru <mtarnaru@yahoo.com>
2020-03-08 11:20:42 +0200

    initial Controller

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,31 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import sample.entity.User;
+
+
 public class Controller {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+    public void loginButtonIsClicked() {
+        User user = new User(username.getText(), password.getText());
+      /*
+        if(userExists(userList, user)){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setContentText("You have logged in succesfully !");
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("You have failed !!!!");
+            alert.show();
+        }
+    }*/
 
 }
