c4502c9ad80d38b4950076ec27550b11d13ae06a
sabiutayo <sabiutayo@yahoo.com>
2020-03-12 15:14:01 +0200

    Added methods

--- src/main/java/sample/ProductDao.java
+++ src/main/java/sample/ProductDao.java
@@ -5,8 +5,21 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import sample.entity.Product;
 
+import java.util.List;
+
 @Data
 public class ProductDao {
+    private Session session = HibernateUtil.getSession();
+
+    public List getAllProducts() {
+        List<Product> products = session.createQuery("from Product").list();
+        return products;
+    }
+
+    public Product getProduct(int productId) {
+        Product product = session.get(Product.class, productId);
+        return product;
+    }
 
     public void removeProduct(Product productToRemove) {
         Session session = HibernateUtil.getSession();
