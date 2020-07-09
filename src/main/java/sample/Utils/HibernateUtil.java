0e1b0732d0ff7495f503d46af605f0516bf233a2
sabiutayo <sabiutayo@yahoo.com>
2020-07-09 13:53:40 +0300

    Updated MySQLDialect

--- src/main/java/sample/Utils/HibernateUtil.java
+++ src/main/java/sample/Utils/HibernateUtil.java
@@ -22,7 +22,7 @@ public class HibernateUtil {
             settings.put(Environment.URL, "jdbc:mysql://localhost:3306/market_place?useSSL=false&serverTimezone=UTC");
             settings.put(Environment.USER, "root");
             settings.put(Environment.PASS, "root");
-            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
             settings.put(Environment.SHOW_SQL, "true");
             settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
             settings.put(Environment.HBM2DDL_AUTO, "update");
